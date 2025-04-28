abfe1ea69c69226b03cd4f9f5b647496ed333a3d
KuldeepSinghji <kuldeepsingh57985@gmail.com>
2025-04-28 18:32:51 +0530

    Intiliaze the userdetailservice project

---
+++ userdetailservice/src/main/java/com/deepfitness/userdetailservice/UserdetailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.deepfitness.userdetailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdetailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdetailserviceApplication.class, args);
+	}
+
+}
---
+++ userdetailservice/src/test/java/com/deepfitness/userdetailservice/UserdetailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepfitness.userdetailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdetailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
